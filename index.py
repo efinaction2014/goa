f4142bc2629049c18ebcf99cf9253611ee4df637
andria <andriaefimovski@gmail.com>
2024-10-30 18:14:03 +0400

    Update and rename README.md to index.py

--- index.py
+++ index.py
@@ -1,50 +1,51 @@
+
 #nomer1
-# for index in range(0,11):
-#   print(index)
+ for index in range(0,11):
+   print(index)
 
 #nomer2
-# for python in range(50,151):
-#     print(python)
+ for python in range(50,151):
+     print(python)
 
 #nomer3
-# for html in range(200,500):
-#     print(html)
+ for html in range(200,500):
+     print(html)
 
 #nomer4
-# for css in range(50):
-#     print(css)
+ for css in range(50):
+     print(css)
 
 #nomer6
 # for ციკლს იმ დროს ვიყენებთ როცა ბევრი თუ ცოტა ციფრის გამოიტანს 
 
 #wile loop
 #nomer1
-# i = 16
-# s = 10
+ i = 16
+ s = 10
 
-# while i > s:
-#     print(s ** 1)
-#     s = s + 1
+ while i > s:
+     print(s ** 1)
+     s = s + 1
 
 #nomer2
-# i = 50
-# while i>0:
-#     print(i - 1)
-#     i = i - 1
+ i = 50
+ while i>0:
+     print(i - 1)
+     i = i - 1
 
 #nomer4
-# i = 10
+ i = 10
 
-# while i < 70:
-#     print(i + 2)
-#     i = i + 2
+ while i < 70:
+     print(i + 2)
+     i = i + 2
 
 #nomer5
-# i = 25
+ i = 25
 
-# while i < 55:
-#     print(i + 1)
-#     i = i + 1
+ while i < 55:
+     print(i + 1)
+     i = i + 1
 
 from turtle import *
 
